2363e617f2e9c6b62967fd537145a3b377ca5069
aseemedakkad <aseemedakkad323@gmail.com>
2021-04-13 15:58:07 +0530

    commit01

--- application/views.py
+++ application/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 def test(request):
     return HttpResponse("javad php")
 def function1(request):
-    return render(request,'pro.html')
+    return render(request,'log-boot.html')
 def func2(request):
     return HttpResponse("hello every one")
 
